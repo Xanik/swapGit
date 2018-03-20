ac4dbc9b31aefd7ba0e1d83e39c7df2806d85a7f
Xanik <sandraike57@gmail.com>
2018-03-20 13:14:29 +0100

    adding branch

---
+++ new.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello</title>
+</head>
+<body>
+
+	<p>how was your day</p>
+	<h1>get it</h1>
+
+</body>
+</html>
---
+++ tile.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>This is the day that the lord has made</title>
+</head>
+<body>
+
+	<p>i will rejoice and be glad he lives</p>
+	
+
+</body>
+</html>
